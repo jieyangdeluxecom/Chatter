abfcdc538280744cfc4a2c4e34f4493b54f19c50
Jie Yang <jie.yang@deluxe.com>
2018-10-06 15:37:29 -0500

    go live

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -2,12 +2,15 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.adapter.jetty :as jetty]
             [hiccup.page :as page]
             [hiccup.form :as form]
-            [ring.middleware.params :refer [wrap-params]]))
+            [ring.util.anti-forgery :as anti-forgery]
+            [environ.core :refer [env]]))
 
 
-(def chat-messages (atom ()));this is a variable
+(def chat-messages (atom ()));this is a variable (def chat-messages (atom '()))
 
 
 
@@ -16,7 +19,11 @@
   [d]
   (page/html5
       [:head
-       [:title "chatter"]]
+       [:title "chatter"]
+       (page/include-css "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css")
+       (page/include-js  "//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js")
+       (page/include-js  "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js")
+       (page/include-css "/chatter.css")]
       [:body
        [:h1 "Our Chat App"]
         [:p
@@ -26,7 +33,7 @@
             "Message: " (form/text-field "msg")
             (form/submit-button "Submit"))]
         [:P
-          [:table
+          [:table#messages.table.table-bordered.table-hover.
           (map (fn [m] [:tr [:td (:name m)] [:td (:message m)]]) d)]]]))
 
 (defn updateMessage
@@ -34,10 +41,22 @@
   [d name new-message]
   (swap! d conj {:name name :message new-message}))
 
+(defn init []
+  (println "chatter is starting"))
+
+
+(defn destroy []
+  (println "chatter is shutting down"))
+
 (defroutes app-routes
   (GET "/" [](generateMssage @chat-messages))
   (POST "/" {input :params}(generateMssage (updateMessage chat-messages
     (get input "name") (get input "msg"))))
+  (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app (wrap-params app-routes))  ; the default one works for Get method, but does not work for the POST method for security reason
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty #'app {:port port :join? false})))
