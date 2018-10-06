a0bd15e4a62dc5b975119a4e9a131b845f74458d
Jie Yang <jie.yang@deluxe.com>
2018-10-06 13:10:22 -0500

    add message

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -1,10 +1,20 @@
 (ns chatter.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [hiccup.page :as page]))
 
+
+(defn generatemssage
+  "this function display message"
+  []
+  (page/html5
+      [:head
+       [:title "chatter"]]
+      [:body
+       [:h1 "Our Chat App"]]))
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [](generatemssage))
   (route/not-found "Not Found"))
 
 (def app
